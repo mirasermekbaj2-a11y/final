6b758ec96562d2eb75a05f4d9d69d74f29c7d454
mirasermekbaj2-a11y <mirasermekbaj2@gmail.com>
2025-12-27 01:11:29 +0500

    Add dto

---
+++ src/main/java/com/example/universityy/dto/CourseDto.java
@@ -0,0 +1,9 @@
+package com.example.universityy.dto;
+
+import lombok.Data;
+
+@Data
+public class CourseDto {
+    private Long id;
+    private String title;
+}
---
+++ src/main/java/com/example/universityy/dto/FacultyDto.java
@@ -0,0 +1,9 @@
+package com.example.universityy.dto;
+
+import lombok.Data;
+
+@Data
+public class FacultyDto {
+    private Long id;
+    private String name;
+}
