e139f8601820904dec0c4139478bde12b5b558eb
mirasermekbaj2-a11y <mirasermekbaj2@gmail.com>
2025-12-27 04:19:06 +0500

    update rep

---
+++ src/main/java/com/example/universityy/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.universityy.repository;
+
+import com.example.university.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
