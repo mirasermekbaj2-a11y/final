7754d377bc64eca055160b0a570ed2d5705ce3cf
mirasermekbaj2-a11y <mirasermekbaj2@gmail.com>
2025-12-27 04:20:38 +0500

    fixed

--- src/main/java/com/example/universityy/repository/UserRepository.java
+++ src/main/java/com/example/universityy/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.example.universityy.repository;
 
-import com.example.university.entity.User;
+import com.example.universityy.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
