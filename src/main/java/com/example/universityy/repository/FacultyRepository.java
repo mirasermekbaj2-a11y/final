3ec2e4ce0d711770e93317c0c275e89d7f04336e
mirasermekbaj2-a11y <mirasermekbaj2@gmail.com>
2025-12-27 00:10:36 +0500

    Update1 repository

--- src/main/java/com/example/universityy/repository/FacultyRepository.java
+++ src/main/java/com/example/universityy/repository/FacultyRepository.java
@@ -1,6 +1,6 @@
 package com.example.universityy.repository;
 
-import com.example.university.entity.Faculty;
+import com.example.universityy.entity.Faculty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/example/universityy/repository/StudentRepository.java
+++ src/main/java/com/example/universityy/repository/StudentRepository.java
@@ -1,6 +1,6 @@
 package com.example.universityy.repository;
 
-import com.example.university.entity.Student;
+import com.example.universityy.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
