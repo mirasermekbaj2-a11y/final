070d4b49a3d632a1184ed5bccb686bbb2ac257d0
mirasermekbaj2-a11y <mirasermekbaj2@gmail.com>
2025-12-26 23:22:03 +0500

    first commit

---
+++ src/main/java/com/example/universityy/UniversityyApplication.java
@@ -0,0 +1,13 @@
+package com.example.universityy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityyApplication.class, args);
+	}
+
+}
