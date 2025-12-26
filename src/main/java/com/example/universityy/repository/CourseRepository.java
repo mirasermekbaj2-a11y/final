689934c2775394de9fbd858aada9d4f0c2e7a6a5
mirasermekbaj2-a11y <mirasermekbaj2@gmail.com>
2025-12-27 00:06:21 +0500

    Add repository

---
+++ src/main/java/com/example/universityy/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.example.universityy.repository;
+
+import com.example.university.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
